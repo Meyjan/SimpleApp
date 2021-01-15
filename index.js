2c711513e369ef658ac8472ba46c0705d3a4f136
Meyjan <saragih.meyer0821@gmail.com>
2021-01-16 00:23:07 +0700

    Fixing a bit of error handling

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const jwt = require('./utils/jwt');
 const { MongoClient } = require('mongodb');
 
 const userRouter = require('./routers/userRouter');
+const ResponseGenerator = require('./utils/responseGenerator');
 
 // Constants
 const PORT = 3000;
@@ -23,7 +24,14 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(jwt());
 
-// Database connection
+// Handling error from jwt authorization
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    ResponseGenerator(res, 401, 'Unauthorized');
+  }
+});
+
+// Passing spawned database connection to controller
 app.use((req, res, next) => {
   req.db = db;
   next();
@@ -34,10 +42,10 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// Routes
+// Routes to controllers
 app.use('', userRouter);
 
-// Initiate
+// Initiate database and listen
 MongoClient.connect(process.env.DB_URL, { useNewUrlParser: true, poolSize: 5, useUnifiedTopology: true }, (err, database) => {
   if (err) throw err;
   db = database.db();
